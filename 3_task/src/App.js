1ce602308ef0bd5105fbaef0c3db32c53a97b494
veronika333 <vera3@inbox.ru>
2020-03-30 11:11:23 +0300

    like button adds number

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    likes : 0
+  }
+
+  addHandler = () => {
+console.log("test");
+this.setState({ likes: this.state.likes + 1 });
+  };
+  render() {
+    return (
+      <div>
+      <h1>Total likes: {this.state.likes}</h1>
+      <button onClick={this.addHandler}>Add like</button>
+      </div>
+    );
+  }
 }
 
 export default App;
