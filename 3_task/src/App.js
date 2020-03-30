b0846bead3f47d313bcff6fc49d98e69b4645642
veronika333 <vera3@inbox.ru>
2020-03-30 12:25:55 +0300

    added buttons

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -10,11 +10,22 @@ class App extends Component {
 console.log("test");
 this.setState({ likes: this.state.likes + 1 });
   };
+
+  removeHandler = () => {
+    console.log("test2");
+    this.setState({ likes: this.state.likes - 1 });
+  };
+  resetHandler = () => {
+    console.log("test3");
+    this.setState({ likes: this.state.likes = 0 });
+  };
   render() {
     return (
       <div>
       <h1>Total likes: {this.state.likes}</h1>
       <button onClick={this.addHandler}>Add like</button>
+      <button onClick={this.removeHandler}>Decrease likes</button>
+      <button onClick={this.resetHandler}>Reset likes</button>
       </div>
     );
   }
