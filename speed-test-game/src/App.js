a396613d3de0903737b422b2148ebfa8521b3795
veronika333 <vera3@inbox.ru>
2020-04-06 19:28:40 +0300

    9th task finished

--- speed-test-game/src/App.js
+++ speed-test-game/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css";
+import Circle from './Circle/Circle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+ class App extends Component {
+   state = {
+     score: 0,
+     current: 0,
+   };
+   pace = 1500;
+   timer = undefined;
+
+next = () => {
+  let nextActive = undefined;
+  do {
+    nextActive = getRndInteger(1, 4);
+} while (nextActive === this.state.current);
+this.setState({
+  current: nextActive,
+});
+this.timer = setTimeout(this.next, this.pace);
+console.log(this.state.current);
+};
+
+   clickHandler = (circleID) => {
+     console.log('i was clicked -', circleID);
+     this.setState({
+       score: this.state.score + 1,
+     })
+   }
+   startHandler = () => {
+     this.next();
+   };
+   endHandler = () => {
+     clearTimeout(this.timer);
+   };
+  render() {
+    return (
+      <div>
+        <h1>Speed Test Game</h1>
+    <p>Your score is: {this.state.score}</p>
+    <main>
+      <Circle 
+      active={this.state.current === 1}
+      buttonColor="yellow" click={this.clickHandler.bind(this, 1)} />
+      <Circle active={this.state.current === 2} buttonColor="yellow" click={this.clickHandler.bind(this, 2)} />
+      <Circle active={this.state.current === 3} buttonColor="yellow" click={this.clickHandler.bind(this, 3)} />
+      <Circle active={this.state.current === 4} buttonColor="yellow" click={this.clickHandler.bind(this, 4)} />
+    </main>
+    <button onClick={this.startHandler}>Start Game</button>
+    <button onClick={this.endHandler}>End Game</button>
+      </div>
+    );
+  }
+}
 export default App;
