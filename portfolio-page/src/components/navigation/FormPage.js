12687c75fa0bae5d5cefd427b9f6880bbcbb291d
veronika333 <vera3@inbox.ru>
2020-05-05 11:47:01 +0300

    form-page added

--- portfolio-page/src/components/navigation/FormPage.js
+++ portfolio-page/src/components/navigation/FormPage.js
@@ -8,7 +8,7 @@ import Button from 'react-bootstrap/Button';
 const FormPage = () => {
     return (
         <Form className="messageForm">
-          <h1>Connect with me</h1><br/>
+          <h1>Create Post</h1><br/>
   <Form.Group controlId="formBasicEmail">
     <Form.Label>Email</Form.Label>
     <Form.Control type="email" placeholder="name@example.com" />
@@ -16,12 +16,20 @@ const FormPage = () => {
   </Form.Group>
 
   <Form.Group controlId="formBasicSubject">
-    <Form.Label>Subject</Form.Label>
-    <Form.Control type="text" placeholder="Type the subject" />
+    <Form.Label>Title</Form.Label>
+    <Form.Control type="text" placeholder="Type the title" />
   </Form.Group>
   <Form.Group controlId="formBasicMessage">
-    <Form.Label>Your message</Form.Label>
-    <Form.Control as="textarea" rows="4" type="text" placeholder="Type your message here..." />
+    <Form.Label>Your post</Form.Label>
+    <Form.Control as="textarea" rows="4" type="text" placeholder="Type your post here..." />
+  </Form.Group>
+  <Form.Group>
+  <Form.Label>Image</Form.Label>
+  <Form.File 
+    id="custom-file"
+    label="Upload image"
+    custom
+  /> 
   </Form.Group>
   <Button variant="info" type="submit">
     Submit
